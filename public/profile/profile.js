4c8832613354e322ef814e1f21b426732705008c
Frederik Søndergaard Jensen <FSJ95@hotmail.dk>
2020-06-18 08:21:02 +0200

    Own profile no msg btn

--- public/profile/profile.js
+++ public/profile/profile.js
@@ -11,15 +11,24 @@ $.get(`/api/users/${username}`, function (data) {
 });
 
 $.get(`/api/posts/user/${username}`, function (data) {
-  var postCount = 0;
-  var pointCount = 0;
-  for (i = 0; i < data.length; i++) {
-    postCount += 1;
-    for (j = 0; j < data[i].points.length; j++) {
-      pointCount += data[i].points[j].points;
-    }
+  $.get('/api/status', (status) => {
+    var postCount = 0;
+    var pointCount = 0;
+    for (i = 0; i < data.length; i++) {
+      postCount += 1;
+      for (j = 0; j < data[i].points.length; j++) {
+        pointCount += data[i].points[j].points;
+      }
 
-  }
-  $('#profilePosts').text(postCount);
-  $('#profilePoints').text(pointCount);
+    }
+    $('#profilePosts').text(postCount);
+    $('#profilePoints').text(pointCount);
+
+    if (status.user.username === username) {
+      $('.ownProfileRemove').remove();
+      $('.ownProfileShow').show();
+
+    };
+  });
+
 });
